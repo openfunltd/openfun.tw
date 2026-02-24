3b1d0937ed89102c7fa007116edec53ca4b26dae
typingmonk <tmonk.hsieh@tmonk.tw>
2026-02-24 15:11:24 +0800

    Add openfun latest news

--- js/latest_news.js
+++ js/latest_news.js
@@ -1,22 +1,53 @@
 $(document).ready(function(){
-  $.getJSON("https://raw.githubusercontent.com/openfunltd/news/refs/heads/main/lawtrace/latest.json", function(data) {
-    data.slice(0, 3).forEach((news) => { addNews('lawtrace', news); });
-  });
-  $.getJSON("https://raw.githubusercontent.com/openfunltd/news/refs/heads/main/lyapi/latest.json", function(data) {
-    data.slice(0, 3).forEach((news) => { addNews('lyapi', news); });
-  });
+  addNews();
 });
 
-function addNews(source, news) {
+function addNews() {
+  const news_urls = [
+    { url: "https://raw.githubusercontent.com/openfunltd/news/refs/heads/main/lawtrace/latest.json", source: "lawtrace" },
+    { url: "https://raw.githubusercontent.com/openfunltd/news/refs/heads/main/lyapi/latest.json", source: "lyapi" },
+    { url: "https://raw.githubusercontent.com/openfunltd/news/refs/heads/main/openfun/latest.json", source: "openfun" }
+  ];
+  const requests = news_urls.map(item => $.getJSON(item.url));
+  $.when(...requests).done(function(...responses) {
+    const allNews = responses.map((res, idx) => {
+      const data = res[0];
+      const source = news_urls[idx].source;
+
+      return data.map(obj => ({
+        ...obj,
+        source: source
+      }));
+    });
+    const mergedNews = allNews.flat().sort((a, b) => {
+      return new Date(b.date) - new Date(a.date);
+    });
+    const latest5News = mergedNews.slice(0, 5);
+    latest5News.forEach(news => {
+      renderNews(news);
+    });
+  })
+  /*
+  .fail(function(err) {
+    console.error("Failed to fetch data:", err);
+  });
+  */
+}
+
+function renderNews(news) {
   let color = '';
   let type = '';
-  if (source === 'lawtrace') {
+  if (news.source === 'lawtrace') {
     color = 'tag--green';
     type = 'lawtrace';
   }
-  if (source === 'lyapi') {
+  if (news.source === 'lyapi') {
     color = 'tag--yellow';
     type = 'lyapi';
+  }
+  if (news.source === 'openfun') {
+    color = 'tag--blue';
+    type = 'openfun';
   }
 
   const itemTemplate = `
